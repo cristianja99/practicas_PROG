d261a26254f9f010cc76864b6f9e507bd0968cc5
ITP1DAM <ITP1DAM@DESKTOP-1FTOOEK>
2020-01-31 12:13:03 +0100

    clase RangoExcepcion , posicioArrayException y Alumno con sus constructores y get/set.

---
+++ src/ejerc_9_excepciones_tema6/Ejerc_9_Excepciones_Tema6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerc_9_excepciones_tema6;
+
+/**
+ *
+ * @author ITP1DAM
+ */
+public class Ejerc_9_Excepciones_Tema6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
